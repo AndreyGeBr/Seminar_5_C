6bce371c92f8d9b49b0c56d9f687e57fb7c7980d
Andrey <a89615076005@yandex.ru>
2022-10-05 15:49:33 +0300

    add task 31

---
+++ Seminar/Task31/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Напишите две функции:
+// найти сумму положительных чисел
+// найти сумму отрицательных чисел
+
+Console.Clear();
+Console.WriteLine("Введите числа через пробел");
+
+string input = Console.ReadLine()!;
+int[] array =  ParseToArray (input);
+
+Console.WriteLine($"Сумма положительных чисел равна {PositiveSum(array)}");
+Console.WriteLine($"Сумма отрицательных чисел равна {NegativeSum(array)}");
+
+
+
+
+
+int[] ParseToArray (string str)
+{
+    string[] stringArray = str.Split(" ");
+    int[] result = new int[stringArray.Length];
+
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = int.Parse(stringArray[i]);
+    }
+
+    return result;
+}
+
+int PositiveSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] > 0)
+       sum += array[i]; 
+    }
+
+    return sum;
+}
+
+int NegativeSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] < 0)
+       sum += array[i]; 
+    }
+
+    return sum;
+}
