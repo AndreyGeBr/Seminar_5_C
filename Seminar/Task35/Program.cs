f7ebd11f8d13141fd24e6c7f6dc5e8258e22bfb8
Andrey <a89615076005@yandex.ru>
2022-10-05 17:35:34 +0300

    add task35 & task37

---
+++ Seminar/Task35/Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите метод, который суммирует 2 массива. Каждый элемент одного массива должен суммироваться с соответствующим элементом из второго массива. 
+// Если длины массивов не равны, то должно появляться сообщение об ошибке
+
+Console.Clear();
+
+int[] array1 = CreateArray();
+FillArray(array1);
+PrintArray(array1);
+int[] array2 = CreateArray();
+FillArray(array2);
+PrintArray(array2);
+if (array1.Length == array2.Length)
+{
+    int[] result = SumArray(array1, array2);
+    PrintArray(result);
+}
+else
+{
+    Console.WriteLine("Массивы разной длины");
+}
+
+
+int[] CreateArray()
+{
+    int[] array = new int [new Random().Next(4, 6)];
+    return array;
+}
+
+void FillArray(int[] num)
+{
+    Random random = new Random();
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = random.Next(0, 10);
+    }
+} 
+
+void PrintArray(int[] num)
+{
+    int count = num.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(num[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(",");
+    }
+    Console.WriteLine();
+} 
+
+int[] SumArray(int[] arr1, int[] arr2)
+{
+    int[] sum = new int[arr1.Length];
+    for (int i = 0; i < arr1.Length; i++)
+    {
+       sum[i] = arr1[i] + arr2[i]; 
+    }
+    return sum;
+}
---
+++ Seminar/Task37/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите метод, который объединяет 2 массива в 1. 
+Console.Clear();
+
+int[] array1 = CreateArray();
+FillArray(array1);
+PrintArray(array1);
+int[] array2 = CreateArray();
+FillArray(array2);
+PrintArray(array2);
+int[] result = AddArray(array1, array2);
+PrintArray(result);
+
+
+int[] CreateArray()
+{
+    int[] array = new int [new Random().Next(4, 6)];
+    return array;
+}
+
+void FillArray(int[] num)
+{
+    Random random = new Random();
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = random.Next(0, 10);
+    }
+} 
+
+void PrintArray(int[] num)
+{
+    int count = num.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(num[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(",");
+    }
+    Console.WriteLine();
+} 
+
+int[] AddArray(int[] arr1, int[] arr2)
+{
+    int[] sum = new int[arr1.Length + arr2.Length];
+    for (int i = 0; i < arr1.Length; i++)
+    {
+       sum[i] = arr1[i]; 
+    }
+     for (int i = arr1.Length; i < (arr1.Length + arr2.Length); i++)
+    {
+       sum[i] = arr2[i - arr1.Length]; 
+    }
+    return sum;
+}
