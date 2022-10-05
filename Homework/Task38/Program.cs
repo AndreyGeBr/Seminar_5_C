7f0a9e424261dba54f84a2b6c7439b091a0d72bd
Andrey <a89615076005@yandex.ru>
2022-10-05 19:48:31 +0300

    add task 38

---
+++ Homework/Task38/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+
+double[] array = CreateArray();
+FillArray(array);
+PrintArray(array);
+double result = FindMax(array) - FindMin(array);
+Console.WriteLine($"Разницу между максимальным и минимальным элементов массива равна {FindMax(array)} - {FindMin(array)} = {result}");
+
+
+double[] CreateArray()
+{
+    Random random = new Random();
+    double[] array = new double [random.Next(4, 11)];
+    return array;
+}
+
+void FillArray(double[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(random.NextDouble() * 10, 2);
+    }
+} 
+
+void PrintArray(double[] arr)
+{
+    int count = arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(arr[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write("  ");
+    }
+    Console.WriteLine();
+} 
+
+double FindMin(double[] arr)
+{    
+    int i = 0;   
+    double min = arr[i];
+    for (i = 0; i < arr.Length; i++) 
+    {
+       if (arr[i] < min)
+       min = arr[i];
+    }
+    return min;
+}
+
+double FindMax(double[] arr)
+{   
+    int i = 0;
+    double max = arr[i];
+    for (i = 0; i < arr.Length; i++) 
+    {
+       if (arr[i] > max)
+       max = arr[i];
+    }
+    return max;
+}
