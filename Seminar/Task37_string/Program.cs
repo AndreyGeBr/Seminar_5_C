a7d34db653e740970ed4e5115e44113b8ce2c821
Andrey <a89615076005@yandex.ru>
2022-10-05 17:50:07 +0300

    add string version for task37

---
+++ Seminar/Task37_string/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите метод, который объединяет 2 массива в 1. 
+// Решение через string
+
+Console.Write("Введите числа первого массива через пробел: ");
+string firstArray = Console.ReadLine()!;
+
+Console.Write("Введите числа второго массива через пробел: ");
+string secondArray = Console.ReadLine()!;
+
+int[] newArray = AddToArray(firstArray, secondArray);
+
+Console.WriteLine(String.Join(", ", newArray));
+
+int[] AddToArray(string str1, string str2)
+{
+    string str = str1 + ' ' + str2;
+    string[] stringArray = str.Split(" ");
+    int[] result = new int[stringArray.Length];
+
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = int.Parse(stringArray[i]);
+    }
+    return result;
+}
